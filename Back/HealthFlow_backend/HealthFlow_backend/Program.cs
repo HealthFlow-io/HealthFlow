f1f740deaa9ef9bb899380a1a24c1dfcec7bee32
Yassine Saadaoui <saadaouiy51@gmail.com>
2026-01-31 23:06:27 +0100

    add swagger to access without the development env

--- Back/HealthFlow_backend/HealthFlow_backend/Program.cs
+++ Back/HealthFlow_backend/HealthFlow_backend/Program.cs
@@ -172,13 +172,14 @@ builder.Services.AddScoped<IFileService, FileService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "HealthFlow API v1");
+        c.RoutePrefix = "swagger";
     });
+if (app.Environment.IsDevelopment())
+{
 }
 
 // Add exception middleware
